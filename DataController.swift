6690c437eb1fc504290c707c64904b420ac26ca5
Grande Variable <>
2024-02-23 21:57:09 +0100

    feat: Ajoute les méthode requise pour s'assurer d'utiliser la base de donnée privé pour l'utilisateur

--- DataController.swift
+++ DataController.swift
@@ -29,6 +29,18 @@ enum StateCheckiCloud {
             }
         }
         
+        // https://arc.net/l/quote/eqskqvnr
+        // allows the view context to automatically merge data synced (imported) from the server.
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        
+        // sets the merge conflict strategy. If this property is not set, Core Data will default to using NSErrorMergePolicy as the conflict resolution strategy (do not handle any conflicts, directly report an error), which will cause iCloud data to not merge correctly into the local database.
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        do {
+              try container.viewContext.setQueryGenerationFrom(.current)
+        } catch {
+             fatalError("Failed to pin viewContext to the current generation:\(error)")
+        }
+        
         checkAccountStatus()
     }
     
