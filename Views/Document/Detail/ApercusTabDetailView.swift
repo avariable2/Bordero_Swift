521db8bd305d13ef673329d3d42f9134b843ce30
Grande Variable <>
2024-04-23 15:53:35 +0200

    feat: Améliore la performance de l'affichage des preview des documents

--- Views/Document/Detail/ApercusTabDetailView.swift
+++ Views/Document/Detail/ApercusTabDetailView.swift
@@ -9,20 +9,32 @@ import SwiftUI
 import PDFKit
 
 struct DocumentApercus : View {
-    var document: Document
+    var document : Document
+    
+    @State var pdfData : Data?
+    
+    init(document: Document) {
+        self.document = document
+        self.pdfData = nil
+    }
     
     var body: some View {
-        if let data = document.contenuPdf {
-            DocumentPDFKitView(document: PDFDocument(data: data))
-        } else {
-            VStack {
-                ContentUnavailableView(
-                    "Erreur de chargement",
-                    systemImage: "doc.viewfinder",
-                    description: Text("Si cela se produit trop souvent, n'hesitez pas à contacter le développeur.").foregroundStyle(.secondary)
-                )
+        VStack {
+            if let data = pdfData {
+                DocumentPDFKitView(document: PDFDocument(data: data))
+            } else {
+                VStack {
+                    ContentUnavailableView(
+                        "Erreur de chargement",
+                        systemImage: "doc.viewfinder",
+                        description: Text("Si cela se produit trop souvent, n'hesitez pas à contacter le développeur.").foregroundStyle(.secondary)
+                    )
+                }
+                .frame(maxWidth: .infinity)
             }
-            .frame(maxWidth: .infinity)
+        }
+        .onAppear() {
+            self.pdfData = document.contenuPdf
         }
     }
 }
