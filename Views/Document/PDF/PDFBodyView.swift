cfcbce0336c056fb12f463582ab8676df49641e4
Grande Variable <>
2024-04-24 01:44:53 +0200

    feat: Restructure la structure du projet pour isoler les view de la création d'un pdf

--- Views/Document/PDF/PDFBodyView.swift
+++ Views/Document/PDF/PDFBodyView.swift
@@ -32,46 +32,6 @@ struct PDFBodyView : View {
     }
 }
 
-struct HeaderPDFView : View {
-    
-    let data: PDFModel
-    
-    var body: some View {
-        HStack(alignment: .top) {
-            VStack(alignment: .leading) {
-                Image(systemName: "apple.logo")
-                    .font(.largeTitle)
-                    .padding(.bottom)
-                
-                if let praticien = data.praticien {
-                    VStack(alignment: .leading) {
-                        Text(praticien.lastname.uppercased()).bold()
-                            + Text(" ")
-                            + Text(praticien.firstname)
-                        
-                        if let coordonne = praticien.adresse1 {
-                            Text(PDFUtils.getRowAdresse(coordonne).formatted(.list(type: .and, width: .narrow)))
-                        }
-                        Text(praticien.phone)
-                        Text(verbatim: praticien.email)
-                            .foregroundStyle(.blue)
-                        Text(verbatim: praticien.website)
-                            .foregroundStyle(.blue)
-                    }
-                    .font(.caption)
-                }
-            }
-            
-            Spacer()
-            
-            Text(data.optionsDocument.typeDocument.rawValue.capitalized)
-                .font(.largeTitle)
-                .foregroundStyle(.secondary)
-        }
-        .font(.callout)
-    }
-}
-
 struct CoutPartView: View {
     
     let remise : Remise
---
+++ Views/Document/PDF/Views/PDFHeaderView.swift
@@ -0,0 +1,52 @@
+//
+//  PDFHeaderView.swift
+//  Bordero
+//
+//  Created by Grande Variable on 24/04/2024.
+//
+
+import SwiftUI
+
+struct HeaderPDFView : View {
+    
+    let data: PDFModel
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading) {
+                Image(systemName: "apple.logo")
+                    .font(.largeTitle)
+                    .padding(.bottom)
+                
+                if let praticien = data.praticien {
+                    VStack(alignment: .leading) {
+                        Text(praticien.lastname.uppercased()).bold()
+                            + Text(" ")
+                            + Text(praticien.firstname)
+                        
+                        if let coordonne = praticien.adresse1 {
+                            Text(PDFUtils.getRowAdresse(coordonne).formatted(.list(type: .and, width: .narrow)))
+                        }
+                        Text(praticien.phone)
+                        Text(verbatim: praticien.email)
+                            .foregroundStyle(.blue)
+                        Text(verbatim: praticien.website)
+                            .foregroundStyle(.blue)
+                    }
+                    .font(.caption)
+                }
+            }
+            
+            Spacer()
+            
+            Text(data.optionsDocument.typeDocument.rawValue.capitalized)
+                .font(.largeTitle)
+                .foregroundStyle(.secondary)
+        }
+        .font(.callout)
+    }
+}
+
+//#Preview {
+//    PDFHeaderView()
+//}
