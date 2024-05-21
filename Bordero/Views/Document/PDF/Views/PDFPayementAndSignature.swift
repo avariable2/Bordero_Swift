c5ad2c1039e1ebaec2aa585fedfaad0894d090ba
Grande Variable <>
2024-05-21 22:36:33 +0200

    feat: Ajoute les paramètres dans la création de document. Cela permet a l'utilisateur d'avoir encore plus de controle

--- Bordero/Views/Document/PDF/Views/PDFPayementAndSignature.swift
+++ Bordero/Views/Document/PDF/Views/PDFPayementAndSignature.swift
@@ -18,17 +18,19 @@ struct PDFPayementAndSignature: View {
                 Text("Commentaires : ")
                     .fontWeight(.semibold)
                 
-                if data.optionsDocument.payementAllow.isEmpty {
-                    Text("Mode de règlement acceptées : Aucun")
-                } else {
-                    Text("Mode de règlement acceptées : \(data.optionsDocument.payementAllow.map { $0.rawValue }.joined(separator: ", "))")
+                if praticien?.paramsDocument.showModePaiement ?? true {
+                    if data.optionsDocument.payementAllow.isEmpty {
+                        Text("Mode de règlement acceptées : Aucun")
+                    } else {
+                        Text("Mode de règlement acceptées : \(data.optionsDocument.payementAllow.map { $0.rawValue }.joined(separator: ", "))")
+                    }
                 }
                 
                 if data.optionsDocument.payementFinish && data.optionsDocument.estFacture {
                     Text("Facture réglée avec : \(data.optionsDocument.payementUse.rawValue)")
                 }
                 
-                if data.optionsDocument.afficherDateEcheance {
+                if data.optionsDocument.afficherDateEcheance && (praticien?.paramsDocument.showDateEcheance ?? true) {
                     Text("Date d'échéance : \(data.optionsDocument.dateEcheance.formatted(date: .numeric, time: .omitted))")
                 }
                 
@@ -44,11 +46,10 @@ struct PDFPayementAndSignature: View {
                 if let adresse1 = praticien?.adresse1, let ville : String = adresse1["ville"] as? String, !ville.isEmpty {
                     Text("A \(ville), le \(data.optionsDocument.dateEmission.formatted(date: .numeric, time: .omitted)),")
                         .padding()
+                } else {
+                    Text("Le \(data.optionsDocument.dateEmission.formatted(date: .numeric, time: .omitted)),")
+                        .padding()
                 }
-                else {
-                   Text("Le \(data.optionsDocument.dateEmission.formatted(date: .numeric, time: .omitted)),")
-                       .padding()
-               }
                 
                 if let sign = praticien?.signature, let uiImage = UIImage(data: sign) {
                     Image(uiImage: uiImage)
@@ -57,7 +58,7 @@ struct PDFPayementAndSignature: View {
                         .frame(width: 200)
                         .frame(maxHeight: 60)
                 }
-                    
+                
             }
         }
         .font(.caption)
