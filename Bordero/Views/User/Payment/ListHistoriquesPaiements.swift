335f868d21d82e01460827caaab07f8afcb7970f
Grande Variable <gvariable@icloud.com>
2024-06-06 15:27:23 +0200

    feat: Modifie la structure pour être plus facilement importable

--- Bordero/Views/User/Payment/ListHistoriquesPaiements.swift
+++ Bordero/Views/User/Payment/ListHistoriquesPaiements.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import TokenTextField
 
 struct ListHistoriquesPaiements: View {
     @Environment(\.managedObjectContext) var moc
@@ -32,13 +33,11 @@ struct ListHistoriquesPaiements: View {
             .sheet(item: $activeSheet) { activeSheet in
                 switch activeSheet {
                 case .showAllHistoriquePaiement:
-                    NavigationStack {
-                        ListAllClientPaiements(payments: payments)
-                    }
+                    ListAllClientPaiements(payments: payments)
                 case .showDetailPaiement(paiement: let paiement):
                     NavigationView {
                         DisplayPayementSheet(paiement: paiement)
-                            
+                        
                     }
                     .presentationDetents([.medium, .large])
                 default:
@@ -61,10 +60,6 @@ struct RowHistoriquePaiements : View {
             TextPaiementView(payment: payment)
         }
     }
-}
-
-#Preview {
-    ListHistoriquesPaiements()
 }
 
 struct TextPaiementView: View {
@@ -90,25 +85,91 @@ struct TextPaiementView: View {
 struct ListAllClientPaiements: View {
     @Environment(\.dismiss) var dismiss
     @State var payments : FetchedResults<Paiement>
+    @State private var searchText = ""
+    @State private var selectedTokens: [SearchToken] = []
     
-    var body: some View {
-        List(payments) { payment in
-            NavigationLink {
-                DisplayPayementSheet(paiement: payment)
-            } label: {
-                TextPaiementView(payment: payment)
+    var filteredPayments: [Paiement] {
+        var paymentsFilter = Array(self.payments)
+        if selectedTokens.count > 0 {
+            paymentsFilter = payments.filter { payment in  payment.client?.lastname.localizedCaseInsensitiveContains(searchText) ?? false
             }
         }
-        .trackEventOnAppear(event: .paymentListBrowsed, category: .paymentManagement)
-        .navigationTitle("Historique paiements")
-        .toolbar {
-            ToolbarItem(placement: .cancellationAction) {
-                Button {
-                    dismiss()
+        if selectedTokens.count > 0 {
+            let tokens = selectedTokens.map { $0.value }
+//            paymentsFilter = payments.filter { payment in
+//                payment.client?.lastname.contains(<#T##regex: RegexComponent##RegexComponent#>)
+//            }
+        }
+        return paymentsFilter
+    }
+    
+    var body: some View {
+        NavigationStack {
+            
+            List(payments) { payment in
+                NavigationLink {
+                    DisplayPayementSheet(paiement: payment)
                 } label: {
-                    Text("Retour")
+                    TextPaiementView(payment: payment)
+                }
+            }
+            .searchable(text: $searchText, tokens: $selectedTokens, prompt: "Rechercher par nom de client ou date") { token in
+                Text(token.value)
+            }
+            .trackEventOnAppear(event: .paymentListBrowsed, category: .paymentManagement)
+            .navigationTitle("Historique paiements")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("Retour")
+                    }
                 }
             }
         }
     }
+}
+
+struct TokenView: View {
+    var token: SearchToken
+    var removeAction: () -> Void
+    
+    var body: some View {
+        HStack {
+            Text(token.value)
+                .padding(.vertical, 4)
+                .padding(.horizontal, 8)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+            Button(action: removeAction) {
+                Image(systemName: "xmark.circle.fill")
+                    .foregroundColor(.gray)
+                    .padding(.leading, 4)
+            }
+        }
+    }
+}
+
+struct SearchToken: Identifiable, Hashable {
+    let id = UUID()
+    var value: String
+    var type: TokenType
+    
+    static func testData() -> [SearchToken] {
+        [
+            SearchToken(value: "John Appleseed", type: .client),
+            SearchToken(value: "Kate Bell", type: .client),
+            SearchToken(value: "16/05/2024", type: .date)
+        ]
+    }
+}
+
+enum TokenType {
+    case client
+    case date
+}
+
+#Preview {
+    ListHistoriquesPaiements()
 }
